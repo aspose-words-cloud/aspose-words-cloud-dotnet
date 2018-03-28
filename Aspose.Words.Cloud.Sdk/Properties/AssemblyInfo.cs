ceef94a0268b4155a7f4541d102a08570dbffd16
Evgeny Ivanov <Evgeny.Ivanov@aspose.com>
2018-03-28 11:33:36 +0700

    updated version and release notes

--- Aspose.Words.Cloud.Sdk/Properties/AssemblyInfo.cs
+++ Aspose.Words.Cloud.Sdk/Properties/AssemblyInfo.cs
@@ -24,7 +24,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -53,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("17.11.0.0")]
-[assembly: AssemblyFileVersion("17.11.0.0")]
+[assembly: AssemblyVersion("18.3.0.0")]
+[assembly: AssemblyFileVersion("18.3.0.0")]

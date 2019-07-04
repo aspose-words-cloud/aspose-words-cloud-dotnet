c1436b75070a576422d201ba694d8f71107facb5
Auckland GitLab <evgeny.ivanov@aspose.com>
2019-07-04 03:23:05 -0700

    SDK regenerated by CI server

--- Aspose.Words.Cloud.Sdk/Model/Requests/UpdateTablePropertiesRequest.cs
+++ Aspose.Words.Cloud.Sdk/Model/Requests/UpdateTablePropertiesRequest.cs
@@ -42,8 +42,8 @@ namespace Aspose.Words.Cloud.Sdk.Model.Requests
         /// Initializes a new instance of the <see cref="UpdateTablePropertiesRequest"/> class.
         /// </summary>
         /// <param name="name">The document name.</param>
-        /// <param name="nodePath">Path to the node, which contains tables.</param>
         /// <param name="index">Object index.</param>
+        /// <param name="nodePath">Path to the node, which contains tables.</param>
         /// <param name="folder">Original document folder.</param>
         /// <param name="storage">Original document storage.</param>
         /// <param name="loadEncoding">Encoding that will be used to load an HTML (or TXT) document if the encoding is not specified in HTML.</param>
@@ -52,11 +52,11 @@ namespace Aspose.Words.Cloud.Sdk.Model.Requests
         /// <param name="revisionAuthor">Initials of the author to use for revisions.If you set this parameter and then make some changes to the document programmatically, save the document and later open the document in MS Word you will see these changes as revisions.</param>
         /// <param name="revisionDateTime">The date and time to use for revisions.</param>
         /// <param name="properties">The properties.</param>
-        public UpdateTablePropertiesRequest(string name, string nodePath, int? index, string folder = null, string storage = null, string loadEncoding = null, string password = null, string destFileName = null, string revisionAuthor = null, string revisionDateTime = null, TableProperties properties = null)             
+        public UpdateTablePropertiesRequest(string name, int? index, string nodePath, string folder = null, string storage = null, string loadEncoding = null, string password = null, string destFileName = null, string revisionAuthor = null, string revisionDateTime = null, TableProperties properties = null)             
         {
             this.Name = name;
-            this.NodePath = nodePath;
             this.Index = index;
+            this.NodePath = nodePath;
             this.Folder = folder;
             this.Storage = storage;
             this.LoadEncoding = loadEncoding;
@@ -73,14 +73,14 @@ namespace Aspose.Words.Cloud.Sdk.Model.Requests
         public string Name { get; set; }
 
         /// <summary>
-        /// Path to the node, which contains tables.
-        /// </summary>  
-        public string NodePath { get; set; }
-
-        /// <summary>
         /// Object index.
         /// </summary>  
         public int? Index { get; set; }
+
+        /// <summary>
+        /// Path to the node, which contains tables.
+        /// </summary>  
+        public string NodePath { get; set; }
 
         /// <summary>
         /// Original document folder.

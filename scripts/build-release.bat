55a3e235985f8fe9de9514255a504e0ff08701b9
yaroslawekimov <yaroslaw.ekimov@aspose.com>
2020-09-17 13:53:19 +0700

    fix script

--- scripts/build-release.bat
+++ scripts/build-release.bat
@@ -5,8 +5,8 @@ mkdir c:\Build\package\lib\netstandard2.0 || goto end
 mkdir c:\Build\package\License || goto end
 
 :Build a release
-c:\build\.nuget\NuGet.exe restore c:\build\Aspose.Words.Cloud.Sdk.sln || goto end
-msbuild c:\build\Aspose.Words.Cloud.Sdk.sln /p:Configuration=Release || goto end
+dotnet restore c:\build\Aspose.Words.Cloud.Sdk.sln || goto end
+dotnet build c:\build\Aspose.Words.Cloud.Sdk.sln -c Release || goto end
 
 :Create a strong name PE .net 2.0 library and sign it
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\Bin\NETFX 4.6.1 Tools\x64\ildasm" c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\net20\Aspose.Words.Cloud.Sdk.dll /out=c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\Aspose.Words.Cloud.Sdk.il || goto end
@@ -45,8 +45,8 @@ dotnet remove c:\Build\Aspose.Words.Cloud.Sdk.BddTests\Aspose.Words.Cloud.Sdk.Bd
 dotnet add c:\Build\Aspose.Words.Cloud.Sdk.BddTests\Aspose.Words.Cloud.Sdk.BddTests.csproj package Aspose.Words-Cloud -s c:\Build\packages -v %SDK_VERSION%.0 || goto end
 
 :Build tests
-c:\build\.nuget\NuGet.exe restore c:\build\Aspose.Words.Cloud.Sdk.sln || goto end
-msbuild c:\build\Aspose.Words.Cloud.Sdk.sln
+dotnet restore c:\build\Aspose.Words.Cloud.Sdk.sln || goto end
+dotnet build c:\build\Aspose.Words.Cloud.Sdk.sln
 
 :end
 exit /b %ERRORLEVEL%

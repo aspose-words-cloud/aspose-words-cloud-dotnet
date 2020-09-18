75290246891ea281766db98167a940abbcc7005a
yaroslawekimov <yaroslaw.ekimov@aspose.com>
2020-09-18 08:43:58 +0700

    check other ilasm

--- scripts/build-release.bat
+++ scripts/build-release.bat
@@ -11,7 +11,7 @@ msbuild c:\build\Aspose.Words.Cloud.Sdk.sln /p:Configuration=Release || goto end
 
 :Create a strong name PE .net standard 2.0 library and sign it
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\Bin\NETFX 4.6.1 Tools\x64\ildasm" c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\netstandard2.0\Aspose.Words.Cloud.Sdk.dll /out=c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\Aspose.Words.Cloud.Sdk.il || goto end
-"C:\Windows\Microsoft.NET\Framework\v2.0.50727\ilasm" c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\Aspose.Words.Cloud.Sdk.il /res=c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\Aspose.Words.Cloud.Sdk.res /dll /key=c:\Build\Scripts\aspose.snk /out=c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\Aspose.Words.Cloud.Sdk.dll || goto end
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\ilasm" c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\Aspose.Words.Cloud.Sdk.il /res=c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\Aspose.Words.Cloud.Sdk.res /dll /key=c:\Build\Scripts\aspose.snk /out=c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\Aspose.Words.Cloud.Sdk.dll || goto end
 c:\\build\tools\\signtool.exe sign /f c:\Build\Scripts\aspose.pfx /fd sha256 /p f27Hp99Ds3 /tr http://timestamp.comodoca.com/?td=sha256 /td sha256 /v c:\Build\Aspose.Words.Cloud.Sdk\bin\Release\Aspose.Words.Cloud.Sdk.dll || goto end
 
 :Check the strong name and the signature for .net standard 2.0 library

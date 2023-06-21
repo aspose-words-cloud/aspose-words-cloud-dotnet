e6b48ad8e5c1979591fc29ce8272e8659818e982
jenkins <jenkins.aspose@gmail.com>
2023-06-21 10:00:31 -0500

    SDK regenerated by CI server [ci skip]

--- Aspose.Words.Cloud.Sdk/Model/FormField.cs
+++ Aspose.Words.Cloud.Sdk/Model/FormField.cs
@@ -36,7 +36,7 @@ namespace Aspose.Words.Cloud.Sdk.Model
     /// <summary>
     /// FromField.
     /// </summary>
-    public class FormField : NodeLink
+    public abstract class FormField : NodeLink
     {
         /// <summary>
         /// Gets or sets a value indicating whether references to the specified form field are automatically updated whenever the field is exited.

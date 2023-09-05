f22281c149d56407f4fd143fd43086f9de95c30d
jenkins <jenkins.aspose@gmail.com>
2023-09-05 07:27:41 -0500

    SDK regenerated by CI server [ci skip]

--- Aspose.Words.Cloud.Sdk/Internal/ApiInvoker.cs
+++ Aspose.Words.Cloud.Sdk/Internal/ApiInvoker.cs
@@ -58,7 +58,7 @@ namespace Aspose.Words.Cloud.Sdk
         public ApiInvoker(List<IRequestHandler> requestHandlers, int timeout)
         {
             this.AddDefaultHeader(AsposeClientHeaderName, ".net sdk");
-            this.AddDefaultHeader(AsposeClientVersionHeaderName, "23.8");
+            this.AddDefaultHeader(AsposeClientVersionHeaderName, "23.9");
             this.requestHandlers = requestHandlers;
             this.HttpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(timeout), };
         }
